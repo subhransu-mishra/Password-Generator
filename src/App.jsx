741473eb2752bb16ef76a4891da4bc466dec7240
subhransu mishra <msubhransu1@gmail.com>
2024-07-19 23:12:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length,setLength] = useState("8");
+  const [allowNumber, setallowNumber] = useState("false");
+  const [allCharacter,setallowCharacter] = useState("false");
+
+  const passGenerate = ()=>{}
+  return (
+    <>
+     <h1>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
