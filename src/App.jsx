dc595a2a9221eb7f04ff62a35254a80bcca98d3a
Subhransu sekhar Mishra <152766746+subhransu-mishra@users.noreply.github.com>
2024-07-28 00:40:55 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback } from 'react';
+import './App.css';
 
 function App() {
-  const [length,setLength] = useState("8");
-  const [allowNumber, setallowNumber] = useState("false");
-  const [allCharacter,setallowCharacter] = useState("false");
+  const [length, setLength] = useState("8");
+  const [allowNumber, setAllowNumber] = useState(false);
+  const [allowCharacter, setAllowCharacter] = useState(false);
+  const [pass, setPass] = useState("");
 
-  const passGenerate = ()=>{}
+  const passwordGenerator = useCallback(() => {
+    let password = '';
+    let character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (allowNumber) character += '0123456789';
+    if (allowCharacter) character += '!@#$%^&*()_+';
+    for (let i = 0; i < length; i++) {
+      let gcharacter = Math.floor(Math.random() * character.length);
+      password += character.charAt(gcharacter);
+    }
+    setPass(password);
+  }, [length, allowNumber, allowCharacter]);
+
+
+
   return (
     <>
-     <h1 className='text-4xl text-center text-black'>Password Generator</h1>
+      <h1 className='text-4xl text-center text-black'>Password Generator</h1>
+      <div className='w-full max-w-full mx-auto shadow-md rounded-lg px-4 my-8 text-green-600 bg-gray-800' id='main-div'>
+        
+
+          <input type='text' placeholder='password' value={pass} className='input-field'></input>
+          <button onClick={passwordGenerator} className='btn'>Generate</button>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
