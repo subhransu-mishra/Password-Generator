7be2893603ba6f1c5fd3693e10742d75472f08fc
Subhransu sekhar Mishra <152766746+subhransu-mishra@users.noreply.github.com>
2024-07-29 14:29:04 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback,useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -18,7 +18,9 @@ function App() {
     }
     setPass(password);
   }, [length, allowNumber, allowCharacter]);
-
+    useEffect(() => {
+     passwordGenerator(); 
+    }, [passwordGenerator]); 
 
 
   return (
@@ -27,8 +29,36 @@ function App() {
       <div className='w-full max-w-full mx-auto shadow-md rounded-lg px-4 my-8 text-green-600 bg-gray-800' id='main-div'>
         
 
-          <input type='text' placeholder='password' value={pass} className='input-field'></input>
-          <button onClick={passwordGenerator} className='btn'>Generate</button>
+          <input type='text' placeholder='password' value={pass} className='input-field my-7' readOnly ></input>
+          <button className='btn'>Copy</button>
+          <div>
+            <input type='range' min={8} max={50} value={length} onChange={(e)=>{setLength(e.target.value)}}></input>
+            <label className='text-rose-700  text-lg'>Length: {length}</label>
+
+          </div>
+          <div>
+            <input type='checkbox'  
+                defaultChecked = {allowNumber} 
+                onChange = {
+                  ()=>{
+                    setAllowNumber((prev)=>!prev)
+                  }
+                    }>
+            </input>
+            <label className='text-rose-700  ml-4 text-lg'>Allow Number</label>
+          </div>
+          <div>
+            <input type='checkbox'  
+                defaultChecked = {allowCharacter} 
+                onChange = {
+                  ()=>{
+                    setAllowCharacter((prev)=>!prev)
+                  }
+                    }>
+            </input>
+            <label className='text-rose-700  ml-4 text-lg'>Allow Character</label>
+          </div>
+          {/* <button onClick={passwordGenerator} className='btn'>Generate</button> */}
 
       </div>
     </>
